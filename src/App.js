0267724c9363ec41d53854c1b98810af727344d6
Rahul Kumar <dr.rahulk95@gmail.com>
2020-02-26 16:09:21 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <code>JSPanther</code> have fun.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.jspanther.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Goto JSPanther
         </a>
       </header>
     </div>
